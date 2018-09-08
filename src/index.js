a6214f92f84c8b0dc8e8a0ff5490cdfaa30b64c8
willybeans <willwedmedyk@gmail.com>
2018-09-08 14:08:52 -0400

    moved create store to duck/index

--- src/index.js
+++ src/index.js
@@ -2,29 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {Provider} from 'react-redux';
+import { configureStore } from './ducks/index';
 
-import thunkMiddleware from 'redux-thunk';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
-
-import teacher from './ducks/teacher';
-import students from './ducks/students';
-import * as studentActions from './ducks/students';
-import assignments from './ducks/assignments';
-import login from './ducks/login';
-import { login as loginAction } from './ducks/login';
-
-const rootReducer = combineReducers({
-  login: login,
-  students: students,
-  teacher: teacher,
-  assignments: assignments
-});
-
-const store = createStore(rootReducer,
-  {},
-  applyMiddleware(thunkMiddleware, logger)
-);
+const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
